53ac0d1ab6670ba8a99e14ddecb2d53e472f30a6
ankitIITRPR <94774111+ankitIITRPR@users.noreply.github.com>
2022-06-01 22:20:07 +0530

    Create fibonacci.cpp

---
+++ Day2_Recursive_Tree/fibonacci.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+class Solution{
+  public:
+    ll n_fibonacci(int n){
+      if(n==0) return 1;
+      
+    }
+};
+  
+int main(){
+  int t;
+  cin>>t;
+  for(int i=0;i<t;i++){
+    int n;
+    cin>>n;
+    
+    Solution ob;
+    cout<<n_fibonacci(n)<<endl;
+  }
+  return 0;
+}
